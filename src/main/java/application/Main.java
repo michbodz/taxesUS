4a716c4828bffe243f18692abdb9e173123aecc6
Misckq <>
2017-05-31 15:57:49 +0200

    Dodano ikonę aplikacji.

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -8,21 +8,22 @@ import javafx.scene.Scene;
 
 import java.io.File;
 import java.net.URL;
+import javafx.scene.image.Image;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			URL url = new File("src/main/java/application/view/Main.fxml").toURL();
-			Parent root = FXMLLoader.load(url);
-	        Scene scene = new Scene(root, 600, 400);
-	        primaryStage.setTitle("Sales taxes in the United States");
-                primaryStage.setMinHeight(400);
-                primaryStage.setMinWidth(400);
-	        primaryStage.setScene(scene);
-	        primaryStage.show();
-
+                    URL url = new File("src/main/java/application/view/Main.fxml").toURL();
+                    Parent root = FXMLLoader.load(url);
+                    Scene scene = new Scene(root, 600, 400);
+                    primaryStage.setTitle("Sales taxes in the United States");
+                    primaryStage.setMinHeight(400);
+                    primaryStage.setMinWidth(400);
+                    primaryStage.getIcons().add(new Image("file:src/main/java/application/view/logo.png"));
+                    primaryStage.setScene(scene);
+                    primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
